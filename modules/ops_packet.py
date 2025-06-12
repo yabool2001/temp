174ad48889472057d8c2283a9e3a0aec7e1ccf32
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-06-12 21:27:04 +0200

    Add files via upload

---
+++ modules/ops_packet.py
@@ -0,0 +1,20 @@
+import zlib
+import numpy as np
+
+PREAMBLE = [ 0xAA , 0x0F ]
+HEADER = [ 0xAA , 0xAA ]
+
+def create_packet ( payload ) :
+    length_byte = [ len ( payload ) - 1 ]
+    if length_byte < 0 and length_byte > 255 :
+        return None
+    crc32 = zlib.crc32 ( bytes ( payload ) )
+    crc_bytes = list ( crc32.to_bytes ( 4 , 'big' ) )
+    return PREAMBLE + HEADER + length_byte + payload + crc_bytes
+
+def bits_2_bpsk ( bits ) :
+    return np.array ( [ 1.0 if bit else -1.0 for bit in bits ] , dtype = np.int64 )
+
+def create_bpsk_symbols ( packet ) :
+    bits = np.unpackbits ( np.array ( packet , dtype = np.uint8 ) )
+    return bits_2_bpsk ( bits )
