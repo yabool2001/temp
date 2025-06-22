73021f1968e59896f5ffea7de1dfae3f0ff3d031
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-06-22 10:50:48 +0200

    add clipping logs

--- bpsk-complex-packet-txrx-multiple_frames.py
+++ bpsk-complex-packet-txrx-multiple_frames.py
@@ -34,6 +34,8 @@ csv_filename_tx_waveform = "complex_tx_waveform.csv"
 csv_filename_rx_waveform = "complex_rx_waveform.csv"
 csv_filename_tx_symbols = "complex_tx_symbols.csv"
 csv_filename_rx_symbols = "complex_rx_symbols.csv"
+csv_filename_corr_and_filtered_rx_samples = "corr_and_filtered_rx_samples.csv"
+csv_filename_aligned_rx_samples = "aligned_rx_samples.csv"
 
 script_filename = os.path.basename ( __file__ )
 
@@ -80,24 +82,29 @@ def main():
     #rx_samples_filtered = filters.apply_rrc_rx_filter ( rx_samples , SPS , RRC_BETA , RRC_SPAN , False ) # W przyszłości rozważyć implementację tego filtrowania sampli rx
     rx_samples_phase_corrected = corrections.phase_shift_corr ( rx_samples )
     #plot.plot_complex_waveform ( rx_samples_phase_corrected , script_filename + " rx_samples_phase_corrected" )
-    rx_samples_corr_and_filtered = filters.apply_tx_rrc_filter ( rx_samples_phase_corrected , SPS , RRC_BETA , RRC_SPAN , upsample = False ) # Może zmienić na apply_rrc_rx_filter
-    while ( rx_samples_corr_and_filtered.size > 0 ) :
-        corr = np.correlate ( rx_samples_corr_and_filtered , preamble_samples , mode = 'full' )
+    corr_and_filtered_rx_samples = filters.apply_tx_rrc_filter ( rx_samples_phase_corrected , SPS , RRC_BETA , RRC_SPAN , upsample = False ) # Może zmienić na apply_rrc_rx_filter
+    print ( f"{corr_and_filtered_rx_samples.size=}")
+    while ( corr_and_filtered_rx_samples.size > 0 ) :
+        corr = np.correlate ( corr_and_filtered_rx_samples , preamble_samples , mode = 'full' )
         peak_index = np.argmax ( np.abs ( corr ) )
         timing_offset = peak_index - len ( preamble_samples ) + 1
-        aligned_rx_samples = rx_samples_corr_and_filtered[ timing_offset: ]
+        print ( f"{timing_offset=} | ")
+        aligned_rx_samples = corr_and_filtered_rx_samples[ timing_offset: ]
+        print ( f"{aligned_rx_samples.size=}")
         #plot.plot_complex_waveform ( aligned_rx_samples , script_filename + " aligned_rx_samples" )
         if ops_packet.is_preamble ( aligned_rx_samples , RRC_SPAN , SPS ) :
             payload_bits , clip_samples_index = ops_packet.get_payload_bytes ( aligned_rx_samples , RRC_SPAN , SPS )
             print ( f"{payload_bits=}" )
-            rx_samples_corr_and_filtered = aligned_rx_samples[ int ( clip_samples_index ) ::]
+            corr_and_filtered_rx_samples = aligned_rx_samples[ int ( clip_samples_index ) ::]
+            print ( f"{corr_and_filtered_rx_samples.size=}")
         else :
-            print ( "No expected preamble. Breaking!" )
+            print ( "No preamble. Breaking!" )
             break
+        print ( f"{timing_offset=} | ")
     symbols_rx = aligned_rx_samples [ RRC_SPAN * SPS // 2::SPS]
-    plot.plot_bpsk_symbols_v2 ( symbols_rx , script_filename + " symbols_rx" )
+    #plot.plot_bpsk_symbols_v2 ( symbols_rx , script_filename + " symbols_rx" )
     bits_rx = ( symbols_rx.real > 0 ).astype ( int )
-    plot.plot_bpsk_symbols_v2 ( bits_rx , script_filename + " bits_rx" )
+    #plot.plot_bpsk_symbols_v2 ( bits_rx , script_filename + " bits_rx" )
     #print ( f"{bits_rx=}" )
     #if ops_packet.get_preamble ( symbols_rx ) == ops_packet.BARKER13_W_PADDING_UINT16 :
     
@@ -105,18 +112,13 @@ def main():
     acg_vaule = pluto._get_iio_attr ( 'voltage0' , 'hardwaregain' , False )
     # Stop transmitting
 
-    csv_tx_symbols , csv_writer_tx_symbols = ops_file.open_and_write_symbols_2_csv ( csv_filename_tx_symbols , tx_bpsk_symbols )
-    csv_rx_symbols , csv_writer_rx_symbols = ops_file.open_and_write_symbols_2_csv ( csv_filename_rx_symbols , symbols_rx )
-    csv_file_tx , csv_writer_tx = ops_file.open_and_write_samples_2_csv ( csv_filename_tx_waveform , tx_samples )
-    csv_file_rx , csv_writer_rx = ops_file.open_and_write_samples_2_csv ( csv_filename_rx_waveform , aligned_rx_samples )
-    ops_file.flush_data_and_close_csv ( csv_tx_symbols )
-    ops_file.flush_data_and_close_csv ( csv_rx_symbols )
-    ops_file.flush_data_and_close_csv ( csv_file_tx )
-    ops_file.flush_data_and_close_csv ( csv_file_rx )
-    #ops_file.plot_symbols ( csv_filename_tx_symbols )
-    #ops_file.plot_symbols ( csv_filename_rx_symbols )
-    #ops_file.plot_samples ( csv_filename_tx_waveform )
-    #ops_file.plot_samples ( csv_filename_rx_waveform )
+    csv_corr_and_filtered_rx_samples , csv_writer_corr_and_filtered_rx_samples = ops_file.open_and_write_symbols_2_csv ( csv_filename_corr_and_filtered_rx_samples , corr_and_filtered_rx_samples )
+    csv_aligned_rx_samples , csv_writer_aligned_rx_samples = ops_file.open_and_write_symbols_2_csv ( csv_filename_aligned_rx_samples , aligned_rx_samples )
+    ops_file.flush_data_and_close_csv ( csv_corr_and_filtered_rx_samples )
+    ops_file.flush_data_and_close_csv ( csv_aligned_rx_samples )
+    ops_file.plot_symbols ( csv_filename_corr_and_filtered_rx_samples )
+    ops_file.plot_symbols ( csv_filename_aligned_rx_samples )
+
     print ( f"{acg_vaule=}" )
 
 if __name__ == "__main__":
