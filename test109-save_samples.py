e6a539a6e93acf46abdc1a40efd042eb1f019e1d
yabool2001 <mzemlo.pl@gmail.com>
2025-12-13 21:39:09 +0100

    aa

--- test109-save_samples.py
+++ test109-save_samples.py
@@ -10,8 +10,8 @@ barker13 = modulation.get_barker13_bpsk_samples_v0_1_3 ()
 barker13_clipped = modulation.get_barker13_bpsk_samples_v0_1_3 ( clipped = True )
 
 
-plot.complex_waveform_v0_1_6 ( barker13 , f"{barker13.size=}" , False )
-plot.complex_waveform_v0_1_6 ( barker13_clipped , f"{barker13_clipped.size=}" , False )
+plot.complex_waveform_v0_1_6 ( barker13 , f"{barker13.size=}" , True )
+plot.complex_waveform_v0_1_6 ( barker13_clipped , f"{barker13_clipped.size=}" , True )
 
 #ops_file.save_samples_2_npf ( filename , barker13 )
 #ops_file.save_samples_2_npf ( filename_clipped , barker13_clipped )
