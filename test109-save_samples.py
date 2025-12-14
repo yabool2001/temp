f3b121028e64f047dd6dfacba3e69faa3e83f158
yabool2001 <mzemlo.pl@gmail.com>
2025-12-14 07:39:35 +0100

    aa

--- test109-save_samples.py
+++ test109-save_samples.py
@@ -2,17 +2,22 @@ from modules import ops_file , plot , modulation
 from pathlib import Path
 import numpy as np
 
-Path ( "logs" ).mkdir ( parents = True , exist_ok = True )
+Path ( "normalization" ).mkdir ( parents = True , exist_ok = True )
 
 plt = True
 
-samples_tri = np.array ( [ 0 , 1 , 0 , -1 , 0 , 2 , 0 , -2 , 0 , 8 , 0 , -2 , 0 , 2 , 0 , -1 , 0 , 1 , 0 ,  ] , dtype = np.int8 )
-samples_sq = np.array ( [ 0 , 1 , 1 , -1 , -1 , 0  ] , dtype = np.int8 )
+filename_samples_square_sync_sequence = "normalization/samples_square_sync_sequence.npy"
+filename_samples_triangle_sync_sequence = "normalization/samples_triangle_sync_sequence.npy"
+filename_samples_square = "normalization/samples_square.npy"
+filename_samples_triangle = "normalization/samples_triangle.npy"
 
-filename_tri = "np.samples/real_tri_1.npy"
-filename_sq = "np.samples/real_sq_1.npy"
-filename_clipped = "np.samples/barker13_samples_clipped.npy"
-filename = "np.samples/barker13_samples.npy"
+samples_square_sync_sequence = np.array ( [ 0 , 100 , 100 , -100 , -100 , 0  ] , dtype = np.int32 )
+samples_triangle_sync_sequence = np.array ( [ 0 , 100 , 0 , -100 , 0 , 200 , 0 , -200 , 0 , 1000 , 0 , -200 , 0 , 200 , 0 , -100 , 0 , 100 , 0 ] , dtype = np.int32 )
+
+samples_square = np.array ( [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 100 , 100 , -100 , -100 , 0  , 0 , 0 , 0 , 0 , 0 , 0 ] , dtype = np.int32 )
+samples_triangle = np.array ( [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 100 , 0 , -100 , 0 , 200 , 0 , -200 , 0 , 1000 , 0 , -200 , 0 , 200 , 0 , -100 , 0 , 100 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ] , dtype = np.int32 )
+
+
 
 barker13 = modulation.get_barker13_bpsk_samples_v0_1_3 ()
 barker13_clipped = modulation.get_barker13_bpsk_samples_v0_1_3 ( clipped = True )
@@ -21,10 +26,14 @@ barker13_clipped = modulation.get_barker13_bpsk_samples_v0_1_3 ( clipped = True 
 if plt :
     #plot.complex_waveform_v0_1_6 ( barker13 , f"{barker13.size=}" , True )
     #plot.complex_waveform_v0_1_6 ( barker13_clipped , f"{barker13_clipped.size=}" , True )
-    plot.real_waveform ( samples_tri , f"{samples_tri.size=}" , True )
-    plot.real_waveform ( samples_sq , f"{samples_sq.size=}" , True )
+    plot.real_waveform ( samples_square_sync_sequence , f"{samples_square_sync_sequence.size=}" , True )
+    plot.real_waveform ( samples_square , f"{samples_square.size=}" , True )
+    plot.real_waveform ( samples_triangle_sync_sequence , f"{samples_triangle_sync_sequence.size=}" , True )
+    plot.real_waveform ( samples_triangle , f"{samples_triangle.size=}" , True )
 
 #ops_file.save_samples_2_npf ( filename , barker13 )
 #ops_file.save_samples_2_npf ( filename_clipped , barker13_clipped )
-ops_file.save_samples_2_npf ( filename_tri , samples_tri )
-ops_file.save_samples_2_npf ( filename_sq , samples_sq )
+ops_file.save_samples_2_npf ( filename_samples_square_sync_sequence , samples_square_sync_sequence )
+ops_file.save_samples_2_npf ( filename_samples_triangle_sync_sequence , samples_triangle_sync_sequence )
+ops_file.save_samples_2_npf ( filename_samples_square , samples_square )
+ops_file.save_samples_2_npf ( filename_samples_triangle , samples_triangle )
