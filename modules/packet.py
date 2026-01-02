9c0fadd90933df146c3cb46ff470c9bc051f0500
yabool2001 <mzemlo.pl@gmail.com>
2026-01-03 00:28:19 +0200

    aa

--- modules/packet.py
+++ modules/packet.py
@@ -479,7 +479,7 @@ class RxFrames_v0_1_9 :
                 ( -self.samples_filtered.imag , " frame -imag" )
             ]
             for samples_comp , frame_name in check_components :
-                # sprawdz bo zle jest odczytywany crc32 jako 15 15 15 15
+                # W tym miejscu skończyłem w nocy 2.01.2026 sprawdz bo zle jest odczytywany crc32 jako 15 15 15 15,bo to jest chyba stara wersja samples zapisane bez crc32 frame
                 crc32_bytes_read = self.samples2bytes ( samples_comp [ crc32_start_idx : crc32_end_idx ] )
                 crc32_bytes_calculated = self.create_crc32_bytes ( pad_bits2bytes ( self.samples2bits ( samples_comp [ sync_sequence_start_idx : packet_len_end_idx ] ) ) )
                 if ( crc32_bytes_read == crc32_bytes_calculated ).all () :
