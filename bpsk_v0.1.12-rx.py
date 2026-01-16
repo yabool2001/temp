7e5e1e687622ce0564de9aa9dc0f5b119e3c443d
yabool2001 <mzemlo.pl@gmail.com>
2026-01-16 23:51:20 +0100

    aa

--- bpsk_v0.1.12-rx.py
+++ bpsk_v0.1.12-rx.py
@@ -34,9 +34,9 @@ filename = "np.samples/rx_samples_0.1.8_16_c_mode.npy"
 #filename = "np.samples/rx_samples_0.1.8_17_c_mode_full.npy"
 
         
-wrt_filename_npy = "np.samples/rx_samples_log.npy"
-wrt_filename_csv = "samples.csv/rx_samples_log.csv"
-wrt_filename_log = "logs/rx_samples_log.csv"
+wrt_filename_npy = "np.samples/rx_samples_last.npy"
+wrt_filename_csv = "samples.csv/rx_samples_last.csv"
+wrt_filename_log = "logs/rx_samples_last_log.csv"
 
 with open ( wrt_filename_log , "w" ) as wrt_file :
     wrt_file.write ( "time,idx,has_sync_sequence,has_frame,has_packet\n" )
@@ -46,6 +46,7 @@ received_bytes : NDArray[ np.uint8 ] = np.array ( [] , dtype = np.uint8 )
 previous_samples_leftovers : NDArray[ np.complex128 ] = np.array ( [] , dtype = np.complex128 )
 
 real = True
+wrt = True
 
 if real :
     rx_pluto = packet.RxPluto_v0_1_11 ( sn = sdr.PLUTO_RX_SN )
@@ -53,7 +54,7 @@ else :
     rx_pluto = packet.RxPluto_v0_1_11 ()
 
 print ( f"\n{ script_filename= } receiving: {rx_pluto=} { rx_pluto.samples.samples.size= }" )
-while ( len (received_bytes) < 1000 and real ) or ( not real and received_bytes.size == 0 ) :
+while ( len (received_bytes) < 10000 and real ) or ( not real and received_bytes.size == 0 ) :
     if real :
         rx_pluto.samples.rx ( previous_samples_leftovers = previous_samples_leftovers )
     else :
@@ -75,7 +76,12 @@ while ( len (received_bytes) < 1000 and real ) or ( not real and received_bytes.
         received_bytes = np.concatenate ( [ received_bytes , rx_pluto.samples.frames.samples_payloads_bytes ] )
         print ( f"{received_bytes.size=}" )
 
-    if packet.log_packet != "":
+    if wrt :
+        rx_pluto.samples.save_complex_samples_2_npf ( wrt_filename_npy )
+
+    if packet.log_packet != "" :
         with open ( wrt_filename_log , "a" ) as wrt_file :
             wrt_file.write ( packet.log_packet )
         packet.log_packet = ""
+    
+    t.sleep ( 5 )
