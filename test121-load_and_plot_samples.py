0b1913358c02f073292dda377b1f43c3d8085798
yabool2001 <mzemlo.pl@gmail.com>
2026-02-07 11:19:21 +0100

    aa

--- test121-load_and_plot_samples.py
+++ test121-load_and_plot_samples.py
@@ -3,6 +3,11 @@ import numpy as np
 from numpy.typing import NDArray
 
 samples_filename = "np.samples/rx_samples_0.1.14_1500B_01.npy"
+samples_filename1 = "np.samples/rx_samples_0.1.17_issue40_001.0.npy"
+samples_filename2 = "np.samples/rx_samples_0.1.17_issue40_001.1.npy"
 
-samples : NDArray[ np.complex128 ] = ops_file.open_samples_from_npf ( samples_filename )
-plot.complex_waveform_v0_1_6 ( samples , f"{samples_filename} {samples.size=}" )
+
+samples1 : NDArray[ np.complex128 ] = ops_file.open_samples_from_npf ( samples_filename1 )
+samples2 : NDArray[ np.complex128 ] = ops_file.open_samples_from_npf ( samples_filename2 )
+plot.complex_waveform_v0_1_6 ( samples1 , f"{samples_filename1} {samples1.size=}" )
+plot.complex_waveform_v0_1_6 ( samples2 , f"{samples_filename2} {samples2.size=}" )
